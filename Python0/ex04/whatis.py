98e654bff9ad6e8fd2e0c955bfef25e6188e457d
wmillett <williammillette98@gmail.com>
2024-12-08 20:28:27 +0100

    finished ex04

---
+++ Python0/ex04/whatis.py
@@ -0,0 +1,26 @@
+import sys
+
+def check_conditions(args):
+    if len(args) > 1:
+        raise AssertionError("more than one argument is provided")
+    try:
+        int_arg = int(args[0]) 
+    except ValueError:
+        raise AssertionError("argument is not an integer")
+    return int_arg
+
+def main(args):
+    try:
+        if len(args) == 0:
+            return
+        int_arg = check_conditions(args)
+        if int_arg % 2 == 0:
+            print("I'm Even.")
+        else:
+            print("I'm Odd.")
+            
+    except AssertionError as e:
+        print(f"AssertionError: {e}")
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
