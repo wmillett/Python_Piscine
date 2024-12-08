e824e3f6e7b2c956748647d86007020ffbeea462
wmillett <williammillette98@gmail.com>
2024-12-08 19:27:01 +0100

    finished ex03

--- Python0/ex03/NULL_not_found.py
+++ Python0/ex03/NULL_not_found.py
@@ -1,5 +1,19 @@
 def NULL_not_found(object: any) -> int:
-    
-    
-    return 0
-    return 1
+    try:
+        if object is None:
+            print("Nothing: None <class 'NoneType'>")
+        elif isinstance(object, float) and object != object:
+            print("Cheese: nan <class 'float'>")
+        elif object == 0 and isinstance(object, int):
+            print("Zero: 0 <class 'int'>")
+        elif object == '' and isinstance(object, str):
+            print("Empty: <class 'str'>")
+        elif object is False and isinstance(object, bool):
+            print("Fake: False <class 'bool'>")
+        else:
+            print("Type not Found")
+            return 1
+        return 0
+    except Exception as e:
+        print("Error: Unexpected issue occurred")
+        return 1
