852e752d33652e560a4e2d771e2998709a7d0606
William Millette <wmillett@c1b3c3.42berlin.de>
2025-03-03 18:34:09 +0100

    init ex01 py3

---
+++ Python3/ex01/S1E7.py
@@ -0,0 +1,14 @@
+from S1E9 import Character
+
+
+class Baratheon(Character):
+#your code here
+
+class Lannister(Character):
+#your code here
+
+# decorator
+
+
+def create_lannister(your code here):
+#your code here
