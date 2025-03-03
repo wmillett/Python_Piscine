eeb6c04209e02882607c784b53831186a25eb71b
William Millette <wmillett@c1b3c3.42berlin.de>
2025-03-03 03:57:24 +0100

    finished ex00 of py2

--- Python2/ex00/load_csv.py
+++ Python2/ex00/load_csv.py
@@ -1,4 +1,11 @@
+import pandas as pd
 
 
-def load(path: str) -> Dataset: (You have to adapt the type of return according to your library)
-#your code here
+def load(path: str) -> pd.DataFrame:
+    try:
+        df = pd.read_csv(path)
+        print(f"Loading dataset of dimensions {df.shape}")
+        return df
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
