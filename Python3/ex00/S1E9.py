53dfcd1282027f7a26be518824d0a035fd0c87ea
William Millette <wmillett@c1b3c3.42berlin.de>
2025-03-03 15:14:21 +0100

    init ex00 py3

---
+++ Python3/ex00/S1E9.py
@@ -0,0 +1,18 @@
+from abc import ABC, abstractmethod
+
+
+class Character(ABC):
+    """
+    Class for a character
+    """
+
+
+@abstractmethod
+class Stark(Character):
+    """
+    Stark class that inherits from the
+    Character class.
+
+    Attributes:
+
+    """
