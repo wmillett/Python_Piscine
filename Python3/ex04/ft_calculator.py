cc023e878af662d9bcce480de0b71f520b020f5c
William Millette <wmillett@c1b2c1.42berlin.de>
2025-03-05 12:38:55 +0100

    made custom zip for ex04 py3

--- Python3/ex04/ft_calculator.py
+++ Python3/ex04/ft_calculator.py
@@ -1,7 +1,30 @@
+from typing import Generator, Tuple
+
+
 class calculator:
     """
     A calculator that can perform operations on vectors.
     """
+
+    @staticmethod
+    def custom_zip(
+        list1: list[float], list2: list[float]
+    ) -> Generator[Tuple[float, float], None, None]:
+        """
+        Custom zip function to iterate over two lists simultaneously.
+
+        Args:
+            list1 (list of float): The first list.
+            list2 (list of float): The second list.
+
+        Yields:
+            Tuple[float, float]: A tuple containing elements from both lists.
+        """
+        if len(list1) != len(list2):
+            raise ValueError("Both lists must have the same length.")
+
+        for i in range(len(list1)):
+            yield (list1[i], list2[i])
 
     @staticmethod
     def dotproduct(V1: list[float], V2: list[float]) -> None:
@@ -13,7 +36,7 @@ class calculator:
             V2 (list of float): The second vector.
         """
         result = 0
-        for x, y in zip(V1, V2):
+        for x, y in calculator.custom_zip(V1, V2):
             result += float(x) * float(y)
         print(f"Dot product is: {result}")
 
@@ -26,7 +49,8 @@ class calculator:
             V1 (list of float): The first vector.
             V2 (list of float): The second vector.
         """
-        result = [float(x) + float(y) for x, y in zip(V1, V2)]
+        result = (
+            [float(x) + float(y) for x, y in calculator.custom_zip(V1, V2)])
         print(f"Add Vector is: {result}")
 
     @staticmethod
@@ -38,5 +62,6 @@ class calculator:
             V1 (list of float): The first vector.
             V2 (list of float): The second vector.
         """
-        result = [float(x) - float(y) for x, y in zip(V1, V2)]
+        result = (
+            [float(x) - float(y) for x, y in calculator.custom_zip(V1, V2)])
         print(f"Sous Vector is: {result}")
