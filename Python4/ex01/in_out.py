1bf88fa241280ea890eac5a0ace2fd64df9bd8fb
William Millette <wmillett@c3a10c6.42berlin.de>
2025-03-10 03:48:31 +0100

    init ex01 py4

---
+++ Python4/ex01/in_out.py
@@ -0,0 +1,13 @@
+
+
+def square(x: int | float) -> int | float:
+#your code here
+
+
+def pow(x: int | float) -> int | float:
+#your code here
+
+
+def outer(x: int | float, function) -> object:
+    count = 0
+    def inner() -> float:
